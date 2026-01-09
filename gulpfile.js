65acfe154b1158b1109ee2e498f571ca5588bc1f
Kamil Kecki <kamil.kecki@patagonia.com>
2026-01-09 10:02:33 -0800

    add gulp-cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 import { src, dest, series } from "gulp";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
+import cssnano from "gulp-cssnano";
 const sass = gulpSass(dartSass);
 
 const styles = () =>
   src("src/style.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(cssnano())
     .pipe(dest("dist/"));
 
 const copy = () => src("src/manifest.json").pipe(dest("dist/"));
