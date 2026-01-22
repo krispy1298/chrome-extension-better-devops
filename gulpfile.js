daf8ac611bbe6eaeec53263e55daaba2fbec8e22
Kamil Kecki <kamil.kecki@patagonia.com>
2026-01-22 13:40:33 -0800

    config changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 import cssnano from "gulp-cssnano";
 import { rollup } from "gulp-rollup-2";
+import strip from "@rollup/plugin-strip";
+import { minify } from "rollup-plugin-esbuild-minify";
+import sourcemaps from "gulp-sourcemaps";
 
 const sass = gulpSass(dartSass);
 
@@ -16,15 +19,20 @@ const copy = () => src("src/manifest.json").pipe(dest("dist/"));
 
 const js = () =>
   src("src/index.js")
+    .pipe(sourcemaps.init())
     .pipe(
       rollup({
+        plugins:
+          process.env.BUILD_MODE == "production" ? [strip(), minify()] : [],
+        cache: true,
         output: {
           file: "index.js",
           format: "iife",
           sourcemap: true,
         },
-      })
+      }),
     )
+    .pipe(sourcemaps.write("."))
     .pipe(dest("dist"));
 
 export const build = series(styles, copy, js);
